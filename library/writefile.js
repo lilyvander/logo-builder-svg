a1d64becd179c6893bd4ea60ce74efe7a5ecd6f4
Lillian Vanderbloemen <lillvander18@gmail.com>
2024-01-03 18:49:59 -0700

    added a write svg file

---
+++ library/writefile.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+function writeSVGFile (fileName, svgLogo) {
+    try {
+        fs.writeFileSync(fileName, svgLogo);
+        console.log(`File '${fileName}' successfully created.`);
+    } catch (error) {
+        console.error(`Error writing to file '${fileName}': ${error.message}`);    }
+}
+
+module.exports = writeSVGFile;
